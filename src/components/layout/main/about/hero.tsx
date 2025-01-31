00b6e5d9e48496678d72456589d57eaa2a302f36
Biplovkarki <karkibipl@gmail.com>
2025-01-31 12:58:52 +0545

    hero page- complete

--- src/components/layout/main/about/hero.tsx
+++ src/components/layout/main/about/hero.tsx
@@ -4,6 +4,7 @@ function AboutHero() {
   return (
     <div className="relative w-full h-[2000px]">
       <div className="relative h-[60vh] w-full">
+        <div className="absolute inset-0 ">
         <Image
           src="/images/about/abouthero.png"
           alt="Hero"
@@ -11,8 +12,9 @@ function AboutHero() {
           objectFit="cover"
           objectPosition="center"
         />
-        {/* Centered Text */}
-        <div className="absolute h-[60vh] w-full text-center top-0  p-[70px]">
+        </div>
+       
+        <div className="absolute h-[60vh] w-full text-center flex flex-col justify-center items-center">
           
           
           <h1 className="text-white text-8xl font-bold">About Us</h1>
