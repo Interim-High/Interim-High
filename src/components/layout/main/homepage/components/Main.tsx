b311fd769789ccf4731300b2a1931a9eaf15d013
Leah Deshar <leah.desar@gmail.com>
2025-02-01 20:30:13 +0545

    feat: Add college adventure section to homepage

--- src/components/layout/main/homepage/components/Main.tsx
+++ src/components/layout/main/homepage/components/Main.tsx
@@ -15,7 +15,7 @@ function Main() {
         />
       </div>
 
-      <div className="flex flex-col items-center space-y-8">
+      <div className="flex flex-col items-center space-y-8 relative">
         <h1 className="text-5xl font-bold text-gray-900 text-center mb-4">
           DANPHELINK International College
         </h1>
@@ -39,6 +39,46 @@ function Main() {
             Apply Now
           </Link>
         </div>
+      </div>
+
+      <div className="flex justify-center items-center w-full mt-16 px-28">
+        <div className="flex flex-row bg-[#EBE3E3] w-full rounded-xl p-10 gap-6  ">
+          <div className="max-w-lg p-5">
+            <h1 className="text-3xl font-bold text-red-700 mb-4">
+              Your College Adventure
+            </h1>
+            <p className="text-lg text-justify">
+              Embark on a path of personal and professional growth with us,
+              where innovation meets opportunity.
+            </p>
+            <Link
+              href="/apply"
+              className="mt-6 inline-block px-10 py-4 text-lg font-semibold text-orange-500 border-2 border-orange-500 rounded-full transition-all duration-300 ease-in-out transform hover:bg-orange-500 hover:text-white hover:scale-105"
+            >
+              Read More
+            </Link>
+          </div>
+
+          <div>
+            <Image
+              src="/images/about/adventure.png"
+              alt="Academic Adventure"
+              layout="responsive"
+              width={357}
+              height={295}
+              className="object-cover object-center rounded-lg"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <h1>Our Programs</h1>
+        <p>
+          Our programs offer a perfect balance of theoretical knowledge,
+          practical application, and observational learning, creating dynamic
+          and engaging environments beyond traditional classrooms.
+        </p>
       </div>
     </main>
   );
