a305e1e241059e87433b47d637f3c83e1d826192
preranna <koiralapreranaa@gmail.com>
2025-02-14 12:01:34 +0545

    Chores: Changes in partners logo

--- src/components/layout/main/homepage/components/Main.tsx
+++ src/components/layout/main/homepage/components/Main.tsx
@@ -300,28 +300,29 @@ function Main() {
           </div>
         </div>
       </div>
-
+{/* Leadears type */}
       <div className="mx-5 md:mx-28 py-12 my-20 text-center  rounded-3xl ">
         <h1 className="text-5xl font-extrabold text-gray-900 mb-12 tracking-wide">
           Meet Our <span className="text-orange-600">Trusted</span> Partners
         </h1>
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-12">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-12">
           {[
             {
-              name: "Kristina Subedi",
-              role: "Chairperson",
-              img: "/images/home/leaders/leader1.png",
+              img: "/images/home/leaders/Tnvc.png",
+              img_scr: "https://4dvfx.com"
             },
             {
-              name: "Dhurba Subedi",
-              role: "Chairperson",
-              img: "/images/home/leaders/leader2.png",
+              img: "/images/home/leaders/ssarrownex.png",
+              img_scr: "https://www.4dvfxacademy.com/"
             },
             {
-              name: "Deepak Malla",
-              role: "HOD",
-              img: "/images/home/leaders/leader3.png",
+              img: "/images/home/leaders/zuittlogo.png",
+              img_scr: "https://zuitt.co/"
+            },
+            {
+              img: "/images/home/leaders/thinkThank.png",
+              img_scr: "https://www.arrownex.com/"
             },
           ].map((leader, index) => (
             <div
@@ -330,25 +331,17 @@ function Main() {
             >
               <Image
                 src={leader.img}
-                alt={leader.name}
-                width={357}
-                height={295}
+                alt="partner"
+                width={227}
+                height={195}
                 className="object-cover object-center rounded-xl w-full border border-gray-300"
               />
-              <div className="mt-6">
-                <h2 className="text-2xl font-bold text-gray-900">
-                  {leader.name}
-                </h2>
-                <p className="text-lg text-gray-600 font-medium">
-                  {leader.role}
-                </p>
-              </div>
               <div className="absolute inset-0 bg-gradient-to-t from-orange-50 to-transparent rounded-2xl opacity-30"></div>
             </div>
           ))}
         </div>
       </div>
-    </main>
+    </main> 
   );
 }
 
