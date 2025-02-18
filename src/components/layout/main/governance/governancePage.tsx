1b7e9648af164e6ab9c1458f9aebc386fc6a4e9b
Leah Deshar <leah.desar@gmail.com>
2025-02-18 14:40:20 +0545

    chore: Refactor governancePage component formatting

--- src/components/layout/main/governance/governancePage.tsx
+++ src/components/layout/main/governance/governancePage.tsx
@@ -1,16 +1,16 @@
+"use client";
 import Footer from "../../footer/Footer";
 import Header from "../../header/Header";
 import Card from "../about/card";
 import Governancesection from "./components/govermentDashboard";
 import SideNav from "./components/sidenav";
 
-export default function Governance (){
-    return(
-        <div>
-            <Header/>
-            <Governancesection/>
-            <Footer/>
-            
-        </div>
-    )
+export default function Governance() {
+  return (
+    <div>
+      <Header />
+      <Governancesection />
+      <Footer />
+    </div>
+  );
 }
